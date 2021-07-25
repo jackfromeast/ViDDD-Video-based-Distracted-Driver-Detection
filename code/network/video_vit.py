250a2f4987a21e60b34481a9d5266128e0e0ca16
Happyface001 <63159602+Happyface001@users.noreply.github.com>
2021-07-25 11:38:15 +0800

    Update video_vit.py

--- code/network/video_vit.py
+++ code/network/video_vit.py
@@ -13,81 +13,20 @@ class vvit(nn.Module):
         frame_list = []
         x = x.transpose(1,0).contiguous()
         for i in range(self.frame):
-            frame_feature = self.vit(x[i])
-            frame_list.append(frame_feature.unsqueeze(0))
+            input = x[i].permute(0,3,1,2).contiguous()
+
+            frame_feature = self.vit(input)
+
+            frame_list.append(frame_feature.unsqueeze(1))
+
         gru_input = torch.cat(frame_list,dim=1)
-        gru_output, _ = self.gru(gru_input)
-        out = self.dense(gru_output)
+
+        # enc_hidden[-1, :, :]
+        _,gru_output = self.gru(gru_input)
+
+        out = self.dense(gru_output[-1,:,:])
         return out
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if args.weights != "":
-        #     assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
-        #     weights_dict = torch.load(args.weights, map_location=device)
-        #     # 删除不需要的权重
-        #     del_keys = ['head.weight', 'head.bias'] if model.has_logits \
-        #         else ['head.weight', 'head.bias']
-        #     for k in del_keys:
-        #         del weights_dict[k]
-        #     print(model.load_state_dict(weights_dict, strict=False))
-        #
-        # if args.freeze_layers:
-        #     for name, para in model.named_parameters():
-        #         # 除head, pre_logits外，其他权重全部冻结
-        #         if "head" not in name and "pre_logits" not in name:
-        #             para.requires_grad_(False)
-        #         else:
-        #             print("training {}".format(name))
